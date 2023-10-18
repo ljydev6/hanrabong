0f111274c51c54234b082a09ea1ee415fa2c622c
KangJinha <121327609+KangJinha@users.noreply.github.com>
2023-10-18 12:39:08 +0900

    jh 이름입력

--- gitsampletest/src/com/git/test/jh/JhController.java
+++ gitsampletest/src/com/git/test/jh/JhController.java
@@ -1,9 +1,18 @@
 package com.git.test.jh;
 
-public class JhController {
+import java.util.Scanner;
 
+public class JhController {
+	Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("JH 컨트롤러입니다.");
+		JhController jh = new JhController();
+		jh.msg();
 	}
-
+	public void msg() {
+		System.out.print("이름을 입력해주세요 : ");
+		String name = sc.nextLine();
+		
+		System.out.println(name + "님 반갑습니다 :)");
+	}
 }
