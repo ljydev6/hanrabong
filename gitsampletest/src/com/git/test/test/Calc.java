d447eea3ba13a7d9dddaa17d655062bb68be62eb
qrien <qrien@QRIEN-NOTE>
2023-10-18 12:41:14 +0900

    ljy_ 숙제 만들었음

---
+++ gitsampletest/src/com/git/test/test/Calc.java
@@ -0,0 +1,17 @@
+package com.git.test.test;
+
+import java.util.Scanner;
+
+public class Calc {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요");
+		int num1 = sc.nextInt();
+		
+		//진하 숫자 하나 더 받아서 더하기 출력하는 기능 만들기
+		
+		//경현 숫자 하나 더 받아서 num1이랑 빼기 해서 출력하는 기능 만들기
+		
+		// 채화 숫자 하나 더 받아서 num1이랑 곱하기 해서 출력하는 기능 만들기
+	}
+}
