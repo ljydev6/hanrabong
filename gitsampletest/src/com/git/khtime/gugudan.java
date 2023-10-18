dbd36ed7843460145c4a938b2ba51b5dbfe5c3df
ljydev6 <ljydev6@gmail.com>
2023-10-19 08:53:17 +0900

    ljy_수정의 수정

--- gitsampletest/src/com/git/khtime/gugudan.java
+++ gitsampletest/src/com/git/khtime/gugudan.java
@@ -7,6 +7,7 @@ public class gugudan {
 				for(int j =1; j<10; j++) {
 					System.out.println(i+" x "+ j+" = "+ i*j );
 					System.out.println("수정");
+					System.out.println("수정의 수정");
 				}
 			}
 		
