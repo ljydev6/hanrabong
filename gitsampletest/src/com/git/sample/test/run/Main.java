513a46c9841ae9117da8addfad20efa685db2086
ljydev6 <143601553+ljydev6@users.noreply.github.com>
2024-01-30 22:20:22 +0900

    Delete gitsampletest directory

--- gitsampletest/src/com/git/sample/test/run/Main.java
+++
@@ -1,7 +0,0 @@
-package com.git.sample.test.run;
-
-public class Main {
-	public static void main(String[] args) {
-		
-	}
-}
--- gitsampletest/src/com/git/test/ych/Coghk.java
+++
@@ -1,9 +0,0 @@
-package com.git.test.ych;
-
-
-public class Coghk {
-	public static void main(String[] args) {
-		System.out.println("개발중...");
-		System.out.println("ㅎㅎㅎㅎ");
-	}
-}
